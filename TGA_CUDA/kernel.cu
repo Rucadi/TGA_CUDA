06d1f76e7dc3ddfdf46a225d724615d3fcffff81
ruben <ruben@DESKTOP-BN2Q51Q>
2017-05-16 18:13:36 +0200

    FIX and different size

--- TGA_CUDA/kernel.cu
+++ TGA_CUDA/kernel.cu
@@ -33,51 +33,46 @@ inline void __cudaCheckError(const char *file, const int line)
 
 	return;
 }
-__global__ void sobel(unsigned char* imgray, unsigned char* out)
+__global__ void sobel(unsigned char* imgray, unsigned char* out, int SIZE)
 {
 
 	int x = blockDim.x*blockIdx.x + threadIdx.x;
 	int y = blockDim.y*blockIdx.y + threadIdx.y;
-
-
-		unsigned char pixel00 = imgray[(x - 1) * 512 + y - 1];
-		//printf("pixel00: %d\n", pixel00);
-
-		unsigned char pixel01 = imgray[(x - 1) * 512 + y];
-		unsigned char pixel02 = imgray[(x - 1) * 512 + y + 1];
-		unsigned char pixel10 = imgray[(x) *     512 + y - 1];
-		unsigned char pixel12 = imgray[(x) *     512 + y + 1];
-		unsigned char pixel20 = imgray[(x + 1) * 512 + y - 1];
-		unsigned char pixel21 = imgray[(x + 1) * 512 + y];
-		unsigned char pixel22 = imgray[(x + 1) * 512 + y + 1];
-		int vert = (pixel00 + 2 * pixel01 + pixel02) - (pixel20 + 2 * pixel21 + pixel22);
-		int hori = (pixel00 + 2 * pixel10 + pixel20) - (pixel02 + 2 * pixel12 + pixel22);
-		int tot = vert + hori;
-		tot = (tot>60) ? 255 : 0;
-		out[x * 512 + y] = tot;
+	unsigned char pixel00 = imgray[(x - 1) * SIZE + y - 1];
+	unsigned char pixel01 = imgray[(x - 1) * SIZE + y];
+	unsigned char pixel02 = imgray[(x - 1) * SIZE + y + 1];
+	unsigned char pixel10 = imgray[(x) *     SIZE + y - 1];
+	unsigned char pixel12 = imgray[(x) *     SIZE + y + 1];
+	unsigned char pixel20 = imgray[(x + 1) * SIZE + y - 1];
+	unsigned char pixel21 = imgray[(x + 1) * SIZE + y];
+	unsigned char pixel22 = imgray[(x + 1) * SIZE + y + 1];
+	int vert = (pixel00 + 2 * pixel01 + pixel02) - (pixel20 + 2 * pixel21 + pixel22);
+	int hori = (pixel00 + 2 * pixel10 + pixel20) - (pixel02 + 2 * pixel12 + pixel22);
+	int tot = vert + hori;
+	tot = (tot>60) ? 255 : 0;
+	out[x * SIZE + y] = tot;
 	
 }
 
 
-void CPUSobel(unsigned char* imgray, unsigned char* out)
+void CPUSobel(unsigned char* imgray, unsigned char* out, int SIZE)
 {
 	for (int x = 1; x<512; ++x)
 		for (int y = 0; y < 511; ++y)
 		{
-			unsigned char pixel00 = imgray[(x - 1) * 512 + y - 1];
-			//printf("pixel00: %d\n", pixel00);
-			unsigned char pixel01 = imgray[(x - 1) * 512 + y	];
-			unsigned char pixel02 = imgray[(x - 1) * 512 + y + 1];
-			unsigned char pixel10 = imgray[(x)	   * 512 + y - 1];
-			unsigned char pixel12 = imgray[(x)	   * 512 + y + 1];
-			unsigned char pixel20 = imgray[(x + 1) * 512 + y - 1];
-			unsigned char pixel21 = imgray[(x + 1) * 512 + y	];
-			unsigned char pixel22 = imgray[(x + 1) * 512 + y + 1];
+			unsigned char pixel00 = imgray[(x - 1) * SIZE + y - 1];
+			unsigned char pixel01 = imgray[(x - 1) * SIZE + y	];
+			unsigned char pixel02 = imgray[(x - 1) * SIZE + y + 1];
+			unsigned char pixel10 = imgray[(x)	   * SIZE + y - 1];
+			unsigned char pixel12 = imgray[(x)	   * SIZE + y + 1];
+			unsigned char pixel20 = imgray[(x + 1) * SIZE + y - 1];
+			unsigned char pixel21 = imgray[(x + 1) * SIZE + y	];
+			unsigned char pixel22 = imgray[(x + 1) * SIZE + y + 1];
 			int vert = (pixel00 + 2*pixel01 + pixel02) - (pixel20 + 2 * pixel21 + pixel22);
 			int hori = (pixel00 + 2*pixel10 + pixel20) - (pixel02 + 2 * pixel12 + pixel22);
 			int tot = vert + hori;
 			tot = (tot>60) ? 255 : 0;
-			out[x * 512 + y] = tot;
+			out[x * SIZE + y] = tot;
 		}
 
 }
@@ -87,12 +82,12 @@ void serial()
 	image = cvLoadImage("cameraman.png", CV_LOAD_IMAGE_GRAYSCALE);
 	IplImage* h_image2 = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
 
+	int imgsize = cvGetSize(image).height* cvGetSize(image).width;
 
 	unsigned char *output = (unsigned char*)h_image2->imageData;
 	unsigned char *input = (unsigned char*)image->imageData;
 
-	printf("size=%d\n", cvGetSize(image).height* cvGetSize(image).width);
-	CPUSobel(input, output);
+	CPUSobel(input, output, cvGetSize(image).height);
 	cvShowImage("Image", h_image2);
   cvWaitKey();
 }
@@ -120,7 +115,6 @@ void mycuda()
 
 
 	//reservamos espacio en la tg para nuestras imagenes
-
 	cudaMalloc((unsigned char**)&d_input, imgsize);
 	cudaMalloc((unsigned char**)&d_output, imgsize);
 
@@ -131,7 +125,7 @@ void mycuda()
 	dim3 dimBlock(32, 32);
 	dim3 dimGrid(16, 16);
 
-	sobel<<<dimGrid, dimBlock >>> (d_input, d_output);
+	sobel<<<dimGrid, dimBlock >>> (d_input, d_output, cvGetSize(image).height);
 	CudaCheckError();
 
 
